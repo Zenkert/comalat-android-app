0e9f68dd99d309de4b082e78ddead43bb07a8d3b
Kostas Papaioannou <redoc01@hotmail.com>
2016-10-28 10:52:06 +0300

    Integration of Webview with Login

---
+++ app/src/main/java/org/sakaiproject/sakai/Global.java
@@ -0,0 +1,8 @@
+package org.sakaiproject.sakai;
+
+/**
+ * Created by Kostas on 28-Oct-16.
+ */
+
+public class Global {
+}
---
+++ app/src/main/java/org/sakaiproject/sakai/JavaScriptInterface.java
@@ -0,0 +1,8 @@
+package org.sakaiproject.sakai;
+
+/**
+ * Created by Kostas on 27-Oct-16.
+ */
+
+public class JavaScriptInterface {
+}
